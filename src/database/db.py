580aee1134a38fa34bb3812f5789522e39d21406
Michelle Tolfa <michelletolfa@AA000390.local>
2022-10-27 11:17:15 -0700

    Changes to accomodate Mac dev platform

--- src/database/db.py
+++ src/database/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:newpassword@db:5432/pantry_chef_dev"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:newpassword@db:5432/panda_express"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
